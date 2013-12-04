3bd2ce35285153979a651d58712e4e8a90637ed5
jpcheng <cheng.jeanpierre@yahoo.fr>
2013-12-04 23:54:20 +0800

    add front page with the form to enter zipCode

--- src/main/java/com/kerto/weather/HomeController.java
+++ src/main/java/com/kerto/weather/HomeController.java
@@ -1,13 +1,10 @@
 package com.kerto.weather;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,25 +12,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
+@RequestMapping("/")
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String home() {
 		return "home";
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String findWeatherInfosByZipCode(@RequestBody String zipCode,
+			Model model) {
+		return "home";
+	}
+
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,29 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
 <%@ page session="false" %>
 <html>
 <head>
-	<title>Home</title>
+	<title>Weather</title>
+	<script type="text/javascript" src="<c:url value="/resources/jquery/1.6/jquery.js" />"></script>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+	<div id="formsContent">
+		<h2>Weather</h2>
+		<p>
+			Enter a zipcode in order to have weather's informations	
+		</p>
+		<form id="form" method="post" action="/weather/">
+			<fieldset>
+		  		<legend>Weather</legend>
+		  		<label>
+		  			Zip Code
+		 		<label>
+		  		<input type="text" name="zipCode" />
+		  	</fieldset>
+	
+			<p><button type="submit">Submit</button></p>
+		</form>
+	</div>
 </body>
 </html>
